4fb8f3c5df79567302e360d40b2ceb4011c785ba
akorol777 <akorol777@ya.ru>
2014-03-28 00:51:49 +0200

    js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-
+	$(".nav a").click(function (){
+		var page = $(this).attr("href");
+		$('html, body').animate({
+			scrollTop: $(page).offset().top - 61
+		}, 500);
+		return false;
+	});
 });
+
+
